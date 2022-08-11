47ea754e0be8389cf0e9846e06ab4511b2ccdc5f
amit-kanini <amit.ravindra@kanini.com>
2022-08-11 09:16:22 +0530

    todos without routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+import Todo from './components/Todo'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (  <div>
+    <h1>To do</h1>
+    <Todo text="test"/>
+    <Todo text="first"/>
+    <Todo text="final"/>
+   
+  </div>)
+   
+  
 }
 
 export default App;
